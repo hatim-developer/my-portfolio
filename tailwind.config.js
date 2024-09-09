eb2669b2d9243f640b493a1ef675bb819bfac79b
Mohammed Hatim <hatim.developer@gmail.com>
2024-09-09 21:48:34 +0530

    feat: tailwind custom color naming setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      /* DARK COLORS */
+      "all-color": colors.zinc,
+      "body-color": colors.zinc[900],
+      "primary-color": colors.zinc[100],
+      "secondary-color": colors.zinc[300],
+      "neutral-color": colors.zinc[400],
+      "highlight-color": colors.teal[300],
+    },
   },
   plugins: [],
 };
